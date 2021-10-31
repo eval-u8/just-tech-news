8902dd1fef58d369ecaf970522ecb93216353414
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-31 12:11:58 -0500

    finalized

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
 const sequelize = require("../../config/connection");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
+    console.log("======================");
     Post.findAll({
         attributes: [
             "id",
@@ -19,6 +20,20 @@ router.get("/", (req, res) => {
         ],
         order: [["created_at", "DESC"]],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
             {
                 model: User,
                 attributes: ["username"],
@@ -50,6 +65,20 @@ router.get("/:id", (req, res) => {
             ],
         ],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
             {
                 model: User,
                 attributes: ["username"],
@@ -83,14 +112,13 @@ router.post("/", (req, res) => {
         });
 });
 
-// PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then((updatedPostData) => res.json(updatedPostData))
+    Post.upvote(req.body, { Vote, Comment, User })
+        .then((updatedVoteData) => res.json(updatedVoteData))
         .catch((err) => {
             console.log(err);
-            res.status(400).json(err);
+            res.status(500).json(err);
         });
 });
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
-// GET /api/users
+// get all users
 router.get("/", (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll({ attributes: { exclude: ["password"] } })
+    User.findAll({
+        attributes: { exclude: ["password"] },
+    })
         .then((dbUserData) => res.json(dbUserData))
         .catch((err) => {
             console.log(err);
@@ -12,19 +13,36 @@ router.get("/", (req, res) => {
         });
 });
 
-// GET /api/users/1
 router.get("/:id", (req, res) => {
     User.findOne({
         attributes: { exclude: ["password"] },
         where: {
             id: req.params.id,
         },
+        include: [
+            {
+                model: Post,
+                attributes: ["id", "title", "post_url", "created_at"],
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "created_at"],
+                include: {
+                    model: Post,
+                    attributes: ["title"],
+                },
+            },
+            {
+                model: Post,
+                attributes: ["title"],
+                through: Vote,
+                as: "voted_posts",
+            },
+        ],
     })
         .then((dbUserData) => {
             if (!dbUserData) {
-                res.status(404).json({
-                    message: "No user found with this id",
-                });
+                res.status(404).json({ message: "No user found with this id" });
                 return;
             }
             res.json(dbUserData);
@@ -35,7 +53,6 @@ router.get("/:id", (req, res) => {
         });
 });
 
-// POST /api/users
 router.post("/", (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
@@ -63,25 +80,22 @@ router.post("/login", (req, res) => {
             });
             return;
         }
-        // add comment syntax in front of this line in the .then()
+
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: "Incorrect password!" });
             return;
         }
 
         res.json({ user: dbUserData, message: "You are now logged in!" });
-        // res.json({ user: dbUserData });
-
-        // Verify user
     });
 });
 
-// PUT /api/users/1
 router.put("/:id", (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
         individualHooks: true,
         where: {
@@ -101,7 +115,6 @@ router.put("/:id", (req, res) => {
         });
 });
 
-// DELETE /api/users/1
 router.delete("/:id", (req, res) => {
     User.destroy({
         where: {
